11bcc65cabd770204da79ce3f33239533c38a44d
ayogatot <azerino25@gmail.com>
2019-10-12 11:00:04 +0700

    Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int count = 1;
+  void tombolTambah() {
+    setState(() {
+      count = count + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Stateful dan Stateless'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                count.toString(),
+                style: TextStyle(
+                    fontSize: count.toDouble() + 10, color: Colors.indigo),
+              ),
+              RaisedButton(
+                child: Text('ADD'),
+                onPressed: tombolTambah,
+                color: Colors.amber,
+              )
+            ],
+          ),
         ),
       ),
     );
